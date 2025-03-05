514f93468e172e2094875b90706f49686cea1533
callus-corn <mtfm_ymzk@yahoo.co.jp>
2025-03-05 18:57:30 +0000

    Add test

--- internal/tftp/tftp.go
+++ internal/tftp/tftp.go
@@ -2,20 +2,21 @@ package tftp
 
 import (
 	"bytes"
+	"errors"
+	"fmt"
 	"log/slog"
 	"net"
 	"os"
-	"strings"
 )
 
-type dataConn struct {
-	conn net.PacketConn
-	addr net.Addr
-	file *os.File
+type TFTP struct {
+	blocks  [][]byte
+	blockNo int
 }
 
 const UdpMax = 65536
 const blockMax = 65536
+const blockLen = 512
 const OpcRRQ = 1
 const OpcDATA = 3
 const OpcERROR = 5
@@ -28,101 +29,142 @@ var host string
 var srvDir = "/home/ubuntu"
 
 func Listen(address string) error {
-	host = strings.ReplaceAll(address, ":69", "")
-	conn, err := net.ListenPacket("udp", address)
+	var err error = nil
+	host, _, err = net.SplitHostPort(address)
 	if err != nil {
 		return err
 	}
-	go func(conn net.PacketConn) {
-		defer conn.Close()
 
-		udpBuffer := make([]byte, UdpMax)
-		for {
-			_, addr, err := conn.ReadFrom(udpBuffer)
-			if err != nil {
-				logger.Error(err.Error(), "module", "TFTP")
-			}
-			logger.Info("TFTP connection start", "module", "TFTP", "address", addr.String())
-
-			if opc := udpBuffer[1]; opc != OpcRRQ {
-				logger.Error("Illegal TFTP operation form "+addr.String(), "module", "TFTP")
-				response := []byte{0, OpcERROR, 0, IllegalTFTPOperation, 0}
-				conn.WriteTo(response, addr)
-				continue
-			}
-
-			filename := bytes.Split(udpBuffer[2:], []byte{0})[0]
-			path := srvDir + "/" + string(filename)
-			fp, err := os.Open(path)
-			if err != nil {
-				logger.Error(err.Error(), "module", "TFTP")
-				response := []byte{0, OpcERROR, 0, FileNotFound, 0}
-				conn.WriteTo(response, addr)
-				continue
-			}
-
-			conn, err := net.ListenPacket("udp", host+":0")
-			if err != nil {
-				logger.Error(err.Error(), "module", "TFTP")
-			}
-			logger.Info("TFTP send file", "module", "TFTP", "address", addr.String(), "file", path)
-
-			go dataConn{conn, addr, fp}.write()
-		}
-	}(conn)
+	conn, err := net.ListenPacket("udp", address)
+	fmt.Println(conn.LocalAddr().String())
+	if err != nil {
+		return err
+	}
+	go listen(conn)
 
 	return nil
 }
 
-func (d dataConn) write() {
-	defer d.conn.Close()
-	defer d.file.Close()
-
-	blockLen := 512
-	blocks, err := d.readFileAll(blockLen)
-	if err != nil {
-		logger.Error(err.Error(), "module", "TFTP")
-		response := []byte{0, OpcERROR, 0, Accessviolation, 0}
-		d.conn.WriteTo(response, d.addr)
-		return
-	}
-
-	head := []byte{0, OpcDATA, 0, 1}
-	response := append(head, blocks[0]...)
-	d.conn.WriteTo(response, d.addr)
+func listen(conn net.PacketConn) {
+	defer conn.Close()
 
 	udpBuffer := make([]byte, UdpMax)
 	for {
-		_, addr, err := d.conn.ReadFrom(udpBuffer)
+		_, addr, err := conn.ReadFrom(udpBuffer)
+		if err != nil {
+			logger.Error(err.Error(), "module", "TFTP")
+		}
+		logger.Info("TFTP connection start", "module", "TFTP", "address", addr.String())
+
+		if opc := udpBuffer[1]; opc != OpcRRQ {
+			logger.Error("Illegal TFTP operation form "+addr.String(), "module", "TFTP")
+			response := []byte{0, OpcERROR, 0, IllegalTFTPOperation, 0}
+			conn.WriteTo(response, addr)
+			continue
+		}
+
+		tftp, err := RRQ(udpBuffer)
+		if err != nil {
+			logger.Error(err.Error(), "module", "TFTP")
+			response := []byte{0, OpcERROR, 0, FileNotFound, 0}
+			conn.WriteTo(response, addr)
+			continue
+		}
+
+		conn, err := net.ListenPacket("udp", host+":0")
+		if err != nil {
+			logger.Error(err.Error(), "module", "TFTP")
+		}
+		logger.Info("TFTP send file", "module", "TFTP", "address", addr.String())
+
+		go handleTFTP(conn, addr, tftp)
+	}
+}
+
+func handleTFTP(conn net.PacketConn, addr net.Addr, tftp *TFTP) {
+	defer conn.Close()
+
+	block := make([]byte, blockLen)
+	_, err := tftp.Read(block)
+	if err != nil {
+		logger.Error(err.Error(), "module", "TFTP")
+		response := []byte{0, OpcERROR, 0, Accessviolation, 0}
+		conn.WriteTo(response, addr)
+		return
+	}
+	head := []byte{0, OpcDATA, byte(tftp.blockNo >> 8), byte(tftp.blockNo)}
+	response := append(head, block...)
+	conn.WriteTo(response, addr)
+
+	udpBuffer := make([]byte, UdpMax)
+	for {
+		_, ackAddr, err := conn.ReadFrom(udpBuffer)
 		if err != nil {
 			logger.Error(err.Error(), "module", "TFTP")
 			continue
 		}
-		if addr.String() != d.addr.String() {
+		if ackAddr.String() != addr.String() {
+			continue
+		}
+		if err = tftp.ACK(udpBuffer); err != nil {
+			logger.Error(err.Error(), "module", "TFTP")
 			continue
 		}
 
-		ack := (int(udpBuffer[2]) << 8) + int(udpBuffer[3])
-		blockNo := ack + 1
-		head := []byte{0, OpcDATA, byte(blockNo >> 8), byte(blockNo)}
-		response := append(head, blocks[blockNo-1]...)
-		d.conn.WriteTo(response, d.addr)
+		block := make([]byte, blockLen)
+		n, err := tftp.Read(block)
+		if err != nil {
+			logger.Error(err.Error(), "module", "TFTP")
+			response := []byte{0, OpcERROR, 0, Accessviolation, 0}
+			conn.WriteTo(response, addr)
+			return
+		}
+		if n < blockLen {
+			block = block[:n]
+		}
+		head := []byte{0, OpcDATA, byte(tftp.blockNo >> 8), byte(tftp.blockNo)}
+		response := append(head, block...)
+		conn.WriteTo(response, addr)
 	}
 }
 
-func (d dataConn) readFileAll(blockLen int) ([][]byte, error) {
-	buffer := make([][]byte, blockMax)
+func RRQ(p []byte) (*TFTP, error) {
+	filename := bytes.Split(p[2:], []byte{0})[0]
+	path := srvDir + "/" + string(filename)
+	fp, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	blocks := make([][]byte, blockMax)
 	for i := range blockMax {
-		fileBuffer := make([]byte, blockLen)
-		n, err := d.file.Read(fileBuffer)
+		block := make([]byte, blockLen)
+		n, err := fp.Read(block)
 		if n < blockLen {
-			buffer[i] = fileBuffer[:n]
+			blocks[i] = block[:n]
 			break
 		}
 		if err != nil {
 			return nil, err
 		}
-		buffer[i] = fileBuffer
+		blocks[i] = block
 	}
-	return buffer, nil
+
+	return &TFTP{blocks, 1}, nil
+}
+
+func (t *TFTP) ACK(p []byte) error {
+	ack := (int(p[2]) << 8) + int(p[3])
+	if ack == t.blockNo {
+		t.blockNo = ack + 1
+	} else {
+		return errors.New("invalid ACK number")
+	}
+	return nil
+}
+
+func (t *TFTP) Read(p []byte) (n int, err error) {
+	block := t.blocks[t.blockNo-1]
+	copy(p, block)
+	return len(block), nil
 }
--- internal/tftp/tftp_test.go
+++ internal/tftp/tftp_test.go
@@ -1,9 +1,42 @@
 package tftp
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
 )
 
-func TestListen(t *testing.T) {
+func TestRRQ(t *testing.T) {
+	blockLen := 512
+	want := make([]byte, blockLen)
+	data := []byte("abcdefghijklmnopqrstuvwxyz")
+	copy(want[:len(data)], data)
 
+	dir := t.TempDir()
+	path := filepath.Join(dir, "tmp.txt")
+	err := os.WriteFile(path, data, 0644)
+	if err != nil {
+		t.Error(err)
+	}
+	defer os.Remove(path)
+	srvDir = dir
+
+	head := []byte{0, OpcRRQ}
+	rrq := append(head, []byte("tmp.txt")...)
+	tftp, err := RRQ(rrq)
+	if err != nil {
+		t.Error(err)
+	}
+
+	block := make([]byte, blockLen)
+	_, err = tftp.Read(block)
+	if err != nil {
+		t.Error(err)
+	}
+	for i, v := range block {
+		if v != want[i] {
+			println(v, want[i])
+			t.Errorf("Value")
+		}
+	}
 }
